eee5c42e82a6097ab19601a665826a33f4a4e92b
SparkDustJoe <x@x.x>
2020-09-29 16:08:24 -0400

    Initial commit of all code. Unit testing project and console application included to verify against KATs submitted to NIST by the original authors.  All tests PASS as of this commit.

---
+++ GimliManaged/Gimli24.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Numerics;
+#if DEBUG
+using System.Diagnostics;
+#endif
+
+namespace GimliManaged
+{
+    public class Gimli24
+    {
+        /// <summary>
+        /// Internal state of Gimli (and permute/absorb methods) for v1 as proposed to NIST.
+        /// Uses 24 rounds during <see cref="Statev1.Permute"/>
+        /// </summary>
+        public class Statev1
+        {
+            private static readonly UInt32 R_CONST = 0x9e377900;
+            /// <summary>
+            /// The number of bytes in the Rate portion of the Gimli state.
+            /// </summary>
+            public static readonly Int32 RATE_BYTES = 16;
+            private UInt32[] s = new uint[12];  // 384 bits of state stored as 3x4 UInts (48 bytes)         
+
+            /// <summary>
+            /// Construst a new <see cref="Statev1"/> object. 
+            /// Initializes a "Zero" state with no parameters.
+            /// Optionally supply <paramref name="Nonce"/> AND <paramref name="Key"/> for Encryption/Decryption processes.
+            /// </summary>
+            /// <param name="Nonce"></param>
+            /// <param name="Key"></param>
+            public Statev1(byte[] Nonce = null, byte[] Key = null)
+            {
+                if ((Nonce != null && Key == null) ||
+                    (Nonce == null && Key != null))
+                    throw new InvalidOperationException(
+                        "Cannot create a new Statev1 without complete information. Either specify both a Key and Nonce, or neither (creates a zero state).");
+                else if (Nonce != null && Key != null)
+                {
+                    if (Nonce.Length != 16 || Key.Length != 32)
+                        throw new ArgumentOutOfRangeException(null, "If using the Nonce and Key, Nonce must be 16 bytes, and Key must be 32 bytes");
+                }
+                if (Nonce == null || Key == null) return;
+                Buffer.BlockCopy(Nonce, 0, s, 0, 16); // s0 = nonce
+                Buffer.BlockCopy(Key, 0, s, 16, 16); // s1 = key first half
+                Buffer.BlockCopy(Key, 16, s, 32, 16); // s2 = key second half
+                Permute();
+            }
+            private void SmallSwap()
+            {
+                //s0,0, s0,1, s0,2, s0,3 ← s0,1, s0,0, s0,3, s0,2
+                // https://en.wikipedia.org/wiki/XOR_swap_algorithm
+                s[0] ^= s[1]; // left two ints
+                s[1] ^= s[0];
+                s[0] ^= s[1];
+                s[2] ^= s[3]; // right two ints
+                s[3] ^= s[2];
+                s[2] ^= s[3];
+            }
+            private void BigSwap()
+            {
+                //s0,0, s0,1, s0,2, s0,3 ← s0,2, s0,3, s0,0, s0,1
+                s[0] ^= s[2]; // first and third ints
+                s[2] ^= s[0];
+                s[0] ^= s[2];
+                s[1] ^= s[3]; // second and fouth ints
+                s[3] ^= s[1];
+                s[1] ^= s[3];
+            }
+            /// <summary>
+            /// Perform the Gimli operation
+            /// </summary>
+            public void Permute()
+            {
+                UInt32 x, y, z;
+                for (UInt32 r = 24; r > 0; r--)
+                {
+                    for (byte j = 0; j < 4; j++) // sp box
+                    {
+                        x = BitOperations.RotateLeft(s[j], 24);
+                        y = BitOperations.RotateLeft(s[j + 4], 9);
+                        z = s[j + 8];
+                        s[j + 8] = x ^ (z << 1) ^ ((y & z) << 2);
+                        s[j + 4] = y ^ x        ^ ((x | z) << 1);
+                        s[j]     = z ^ y        ^ ((x & y) << 3);
+                    }
+                    // linear layer
+                    if (r % 4 == 0) // every 4th round
+                        SmallSwap();    // s...s...s...s... etc.
+                    if (r % 4 == 2) // every 4th round starting at the 3rd
+                        BigSwap();      // ..S...S...S...S...etc.
+                    if (r % 4 == 0) // xor constant + round num, every 4th round
+                        s[0] ^= R_CONST ^ (r);    // c...c...c...c... etc.
+                }
+            }
+            /// <summary>
+            /// Extract the Rate portion of the State as a byte array length <see cref="RATE_BYTES"/>
+            /// </summary>
+            public byte[] Squeeze()
+            {
+                byte[] result = new byte[RATE_BYTES];
+                Squeeze(result);
+                return result;
+            }
+            /// <summary>
+            /// Extract the Rate portion of the State into a supplied <paramref name="destination"/> byte array at given <paramref name="offset"/>.
+            /// </summary>
+            /// <exception cref="System.ArgumentNullException">destination is null</exception>
+            /// <exception cref="System.ArgumentOutOfRangeException">offset + <see cref="RATE_BYTES"/> goes past end of destination array</exception>
+            public void Squeeze(byte[] destination, int offset = 0)
+            {
+                if (destination == null)
+                    throw new ArgumentNullException("destination");
+                if (offset + RATE_BYTES > destination.Length)
+                    throw new ArgumentOutOfRangeException("Offset goes past end of destination array.");
+                Buffer.BlockCopy(s, 0, destination, offset, RATE_BYTES);
+            }
+            public void AbsorbBlock(ReadOnlySpan<byte> data) // additional data / hashing
+            {
+                byte[] stuff = Squeeze(); // get a copy of the RATE portion of the state
+                for (int i = 0; i < RATE_BYTES; i++)
+                {
+                    stuff[i] ^= data[i]; // merge/xor state (copy) with data
+                }
+                Buffer.BlockCopy(stuff, 0, s, 0, RATE_BYTES); // state is overwritten with merged data
+                Permute();
+            }
+            public void AbsorbLastBlock(ReadOnlySpan<byte> data)
+            {
+                byte[] stuff = Squeeze(); // get a copy of the RATE portion of the state
+                for (int i = 0; i < data.Length % RATE_BYTES; i++)
+                {
+                    stuff[i] ^= data[i]; // merge/xor state (copy) with data
+                }
+                stuff[data.Length % RATE_BYTES] ^= 0x01; // the reference C has 0x1f, everything else has 0x01
+                Buffer.BlockCopy(stuff, 0, s, 0, RATE_BYTES); // state is overwritten with merged data
+                s[11] ^= 0x01000000; // the reference C has 0x80 for the last most byte, everything else has 0x01
+                Permute();
+            }
+            public void MergeBlock(ReadOnlySpan<byte> data, ref Span<byte> destination, int offset) // encryption
+            {
+                byte[] stuff = Squeeze(); // get a copy of the RATE portion of the state
+                for (int i = 0; i < RATE_BYTES; i++)
+                {
+                    stuff[i] ^= data[i]; // merge/xor state (copy) with data
+                    destination[i + offset] = stuff[i]; // data is updated to the new value
+                }
+                Buffer.BlockCopy(stuff, 0, s, 0, RATE_BYTES); // state is overwritten with merged data
+                Permute();
+            }
+            public void MergeLastBlock(ReadOnlySpan<byte> data, ref Span<byte> destination, int offset)
+            {
+                byte[] stuff = Squeeze(); // get a copy of the RATE portion of the state
+                for (int i = 0; i < data.Length % RATE_BYTES; i++)
+                {
+                    stuff[i] ^= data[i]; // merge/xor state (copy) with data
+                    destination[i + offset] = stuff[i]; // data is updated to the new value
+                }
+                stuff[data.Length % RATE_BYTES] ^= 1;
+                Buffer.BlockCopy(stuff, 0, s, 0, RATE_BYTES); // state is overwritten with merged data
+                s[11] ^= 0x01000000;
+                Permute();
+            }
+            public void StripBlock(ReadOnlySpan<byte> data, ref Span<byte> destination, int offset) // decryption
+            {
+                byte[] stuff = Squeeze(); // get a copy of the RATE portion of the state
+                for (int i = 0; i < RATE_BYTES; i++)
+                {
+                    destination[i + offset] = (byte)(data[i] ^ stuff[i]); // merge/xor state (copy) with data at output
+                    stuff[i] = data[i]; // set state to data
+                }
+                Buffer.BlockCopy(stuff, 0, s, 0, RATE_BYTES); // state is overwritten with data
+                Permute();
+            }
+            public void StripLastBlock(ReadOnlySpan<byte> data, ref Span<byte> destination, int offset)
+            {
+                byte[] stuff = Squeeze(); // get a copy of the RATE portion of the state
+                for (int i = 0; i < data.Length % RATE_BYTES; i++)
+                {
+                    destination[i + offset] = (byte)(data[i] ^ stuff[i]); // merge/xor state (copy) with data at output
+                    stuff[i] = data[i]; // set state to data
+                }
+                stuff[data.Length % RATE_BYTES] ^= 1;
+                Buffer.BlockCopy(stuff, 0, s, 0, RATE_BYTES); // state is overwritten with data
+                s[11] ^= 0x01000000;
+                Permute();
+            }
+        }
+    }
+}
---
+++ GimliManaged/Gimli24v1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Text;
+
+namespace GimliManaged
+{
+    public class Gimli24v1
+    {
+        // hashing first (small), AEAD further down this code file
+        public static Int32 TAG_BYTES = 16;
+        public static Int32 HASH_LEN = 32;
+
+        /// <summary>
+        /// Compute a hash of a byte array using Gimli (as proposed to NIST, v1)
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns><see cref="HASH_LEN"/> bytes of Hash result</returns>
+        public static byte[] ComputeHash(ReadOnlySpan<byte> data)
+        {
+            Gimli24.Statev1 state = new Gimli24.Statev1(); // zero state
+            int d_ptr = 0;
+            while (d_ptr + Gimli24.Statev1.RATE_BYTES <= data.Length)
+            {
+                state.AbsorbBlock(data.Slice(d_ptr, Gimli24.Statev1.RATE_BYTES));
+                d_ptr += 16;
+            }
+            state.AbsorbLastBlock(data.Slice(d_ptr, data.Length % Gimli24.Statev1.RATE_BYTES));
+            byte[] result = new byte[32];
+            state.Squeeze(result, 0);
+            state.Permute();
+            state.Squeeze(result, 16);
+            return result;
+        }
+
+        // encryption
+
+        /// <summary>
+        /// Perform AEAD Encryption process on a byte array using a 16-byte <paramref name="Nonce"/> and 32-byte <paramref name="Key"/> using Gimli (as proposed to NIST, v1).
+        /// </summary>
+        /// <remarks>
+        /// Can optionally supply an <paramref name="AdditionalData"/> byte array that will be processed with the <paramref name="Message"/> array.
+        /// The Tag/MAC is appended to the output, length <seealso cref="TAG_BYTES"/> (len Ciphertext = len Message + TAG_BYTES).
+        /// </remarks>
+        /// <param name="Nonce"></param>
+        /// <param name="Key"></param>
+        /// <param name="AdditionalData"></param>
+        /// <param name="Message"></param>
+        /// <returns>The encrypted Message + <see cref="TAG_BYTES"/> bytes of Tag/MAC</returns>
+        public static byte[] AEADEncrypt(byte[] Nonce, byte[] Key, ReadOnlySpan<byte> AdditionalData, ReadOnlySpan<byte> Message)
+        {
+            if ((Nonce?.Length ?? 0) != 16)
+                throw new ArgumentOutOfRangeException("Nonce", "Nonce must be 16 bytes (and not null).");
+            if ((Key?.Length ?? 0) != 32)
+                throw new ArgumentOutOfRangeException("Key", "Key must be 32 bytes (and not null).");
+
+            Gimli24.Statev1 state = new Gimli24.Statev1(Nonce, Key);
+
+            int ad_ptr = 0;
+            while (ad_ptr + Gimli24.Statev1.RATE_BYTES <= AdditionalData.Length)
+            {
+                state.AbsorbBlock(AdditionalData.Slice(ad_ptr, Gimli24.Statev1.RATE_BYTES));
+                ad_ptr += Gimli24.Statev1.RATE_BYTES;
+            }
+            state.AbsorbLastBlock(AdditionalData.Slice(ad_ptr, AdditionalData.Length % Gimli24.Statev1.RATE_BYTES));
+
+            byte[] output = new byte[Message.Length + Gimli24.Statev1.RATE_BYTES];
+            Span<byte> out_slice = new Span<byte>(output);
+            int d_ptr = 0;
+            while (d_ptr + Gimli24.Statev1.RATE_BYTES <= Message.Length)
+            {
+                state.MergeBlock(Message.Slice(d_ptr, Gimli24.Statev1.RATE_BYTES), ref out_slice, d_ptr);
+                d_ptr += Gimli24.Statev1.RATE_BYTES;
+            }
+            state.MergeLastBlock(Message.Slice(d_ptr, Message.Length % Gimli24.Statev1.RATE_BYTES), ref out_slice, d_ptr);
+
+            state.Squeeze(output, output.Length - TAG_BYTES);
+
+            return output;
+        }
+
+        /// <summary>
+        /// Perform AEAD Decryption process on a byte array using a 16-byte <paramref name="Nonce"/> and 32-byte <paramref name="Key"/> using Gimli (as proposed to NIST, v1).
+        /// </summary>
+        /// <remarks>
+        /// Can optionally supply an <paramref name="AdditionalData"/> byte array that will be processed with the <paramref name="Ciphertext"/> array 
+        /// (must be same bytes as was used during <see cref="AEADEncrypt"/>, and should not be null if any AdditionalData was used during encrpytion).
+        /// The Tag/MAC (of length <see cref="TAG_BYTES"/>) is expected to be at the end of the input (len Message = len Ciphertext - TAG_BYTES).
+        /// </remarks>
+        /// <param name="Nonce"></param>
+        /// <param name="Key"></param>
+        /// <param name="AdditionalData"></param>
+        /// <param name="Ciphertext"></param>
+        /// <returns>The original Message OR NULL if verification fails.</returns>
+        public static byte[] AEADDecryptVerify(byte[] Nonce, byte[] Key, ReadOnlySpan<byte> AdditionalData, ReadOnlySpan<byte> Ciphertext)
+        {
+            if ((Nonce?.Length ?? 0) != 16)
+                throw new ArgumentOutOfRangeException("Nonce", "Nonce must be 16 bytes (and not null).");
+            if ((Key?.Length ?? 0) != 32)
+                throw new ArgumentOutOfRangeException("Key", "Nonce must be 32 bytes (and not null).");
+            if (Ciphertext.Length < 16)
+                throw new ArgumentOutOfRangeException("Ciphertext", "Not enough data. Tag might be missing (input len < 16).");
+
+            Gimli24.Statev1 state = new Gimli24.Statev1(Nonce, Key);
+
+            int ad_ptr = 0;
+            while (ad_ptr + Gimli24.Statev1.RATE_BYTES <= AdditionalData.Length)
+            {
+                state.AbsorbBlock(AdditionalData.Slice(ad_ptr, Gimli24.Statev1.RATE_BYTES));
+                ad_ptr += Gimli24.Statev1.RATE_BYTES;
+            }
+            state.AbsorbLastBlock(AdditionalData.Slice(ad_ptr, AdditionalData.Length % Gimli24.Statev1.RATE_BYTES));
+
+            byte[] output = new byte[Ciphertext.Length - TAG_BYTES];
+            Span<byte> out_slice = new Span<byte>(output);
+            int d_ptr = 0;
+            while (d_ptr + Gimli24.Statev1.RATE_BYTES <= output.Length) // last 16 bytes are the tag
+            {
+                state.StripBlock(Ciphertext.Slice(d_ptr, Gimli24.Statev1.RATE_BYTES), ref out_slice, d_ptr);
+                d_ptr += Gimli24.Statev1.RATE_BYTES;
+            }
+            state.StripLastBlock(Ciphertext.Slice(d_ptr, Ciphertext.Length % Gimli24.Statev1.RATE_BYTES), ref out_slice, d_ptr);
+
+            byte[] tag = state.Squeeze();
+            Int32 verify = 0;
+            for (byte v = 0; v < TAG_BYTES; v++)
+                verify |= tag[v] ^ Ciphertext[Ciphertext.Length - TAG_BYTES + v];
+            if (verify == 0)
+                return output;
+            else
+                return null;
+        }
+
+        // encryption where Tag is put separate from the ciphertext
+
+        /// <summary>
+        /// Perform AEAD Encryption process on a byte array using a 16-byte Nonce, 32-byte Key using Gimli (as proposed to NIST, v1).
+        /// </summary>
+        /// <remarks>
+        /// Can optionally supply an <paramref name="AdditionalData"/> byte array that will be processed with the <paramref name="Message"/> array.
+        /// The Tag/MAC is supplied as a separate OUTPUT variable (<paramref name="Tag"/>), length <see cref="TAG_BYTES"/> (len Ciphertext = len Message).
+        /// </remarks>
+        /// <param name="Nonce"></param>
+        /// <param name="Key"></param>
+        /// <param name="AdditionalData"></param>
+        /// <param name="Message"></param>
+        /// <param name="Tag">OUTPUT Tag/MAC of length <see cref="TAG_BYTES"/></param>
+        /// <returns>The encrypted Message.</returns>
+        public static byte[] AEADEncrypt_Detached(byte[] Nonce, byte[] Key, ReadOnlySpan<byte> AdditionalData, ReadOnlySpan<byte> Message, out byte[] Tag)
+        {
+            if ((Nonce?.Length ?? 0) != 16)
+                throw new ArgumentOutOfRangeException("Nonce", "Nonce must be 16 bytes (and not null).");
+            if ((Key?.Length ?? 0) != 32)
+                throw new ArgumentOutOfRangeException("Key", "Nonce must be 16 bytes (and not null).");
+
+            Gimli24.Statev1 state = new Gimli24.Statev1(Nonce, Key);
+
+            int ad_ptr = 0;
+            while (ad_ptr + Gimli24.Statev1.RATE_BYTES <= AdditionalData.Length)
+            {
+                state.AbsorbBlock(AdditionalData.Slice(ad_ptr, Gimli24.Statev1.RATE_BYTES));
+                ad_ptr += Gimli24.Statev1.RATE_BYTES;
+            }
+            state.AbsorbLastBlock(AdditionalData.Slice(ad_ptr, AdditionalData.Length % Gimli24.Statev1.RATE_BYTES));
+
+            byte[] output = new byte[Message.Length]; // tag sent separately
+            Span<byte> out_slice = new Span<byte>(output);
+            int d_ptr = 0;
+            while (d_ptr + Gimli24.Statev1.RATE_BYTES <= Message.Length)
+            {
+                state.MergeBlock(Message.Slice(d_ptr, Gimli24.Statev1.RATE_BYTES), ref out_slice, d_ptr);
+                d_ptr += Gimli24.Statev1.RATE_BYTES;
+            }
+            state.MergeLastBlock(Message.Slice(d_ptr, Message.Length % Gimli24.Statev1.RATE_BYTES), ref out_slice, d_ptr);
+
+            Tag = state.Squeeze();
+
+            return output;
+        }
+
+        /// <summary>
+        /// Perform AEAD Decryption process on a byte array using a 16-byte <paramref name="Nonce"/> and
+        /// 32-byte <paramref name="Key"/> using Gimli (as proposed to NIST, v1), and the <paramref name="Tag"/> output from <see cref="Gimliv1.AEADEncrypt_Detached"/>.
+        /// </summary>
+        /// <remarks>
+        /// Can optionally supply an <paramref name="AdditionalData"/> byte array that will be processed with the <paramref name="Ciphertext"/> array
+        /// (must be same bytes as was used during <see cref="Gimliv1.AEADEncrypt_Detached"/>, and should not be null if any AdditionalData was used 
+        /// during encrpytion).
+        /// </remarks>
+        /// <param name="Nonce"></param>
+        /// <param name="Key"></param>
+        /// <param name="AdditionalData"></param>
+        /// <param name="Tag">Tag/MAC of length <see cref="TAG_BYTES"/></param>
+        /// <param name="Ciphertext"></param>
+        /// <returns>The original Message OR NULL if verification fails.</returns>
+        public static byte[] AEADDecryptVerify_Detached(byte[] Nonce, byte[] Key, ReadOnlySpan<byte> AdditionalData, byte[] Tag, ReadOnlySpan<byte> Ciphertext)
+        {
+            if ((Nonce?.Length ?? 0) != 16)
+                throw new ArgumentOutOfRangeException("Nonce", "Nonce must be 16 bytes (and not null).");
+            if ((Key?.Length ?? 0) != 32)
+                throw new ArgumentOutOfRangeException("Key", "Nonce must be 32 bytes (and not null).");
+            if ((Tag?.Length ?? 0) != TAG_BYTES)
+                throw new ArgumentOutOfRangeException("Tag", $"Tag must be {TAG_BYTES} bytes (and not null).");
+
+            Gimli24.Statev1 state = new Gimli24.Statev1(Nonce, Key);
+
+            int ad_ptr = 0;
+            while (ad_ptr + Gimli24.Statev1.RATE_BYTES <= AdditionalData.Length)
+            {
+                state.AbsorbBlock(AdditionalData.Slice(ad_ptr, Gimli24.Statev1.RATE_BYTES));
+                ad_ptr += Gimli24.Statev1.RATE_BYTES;
+            }
+            state.AbsorbLastBlock(AdditionalData.Slice(ad_ptr, AdditionalData.Length % Gimli24.Statev1.RATE_BYTES));
+
+            byte[] output = new byte[Ciphertext.Length]; // ciphertext is same length as message, tag is separate
+            Span<byte> out_slice = new Span<byte>(output);
+            int d_ptr = 0;
+            while (d_ptr + Gimli24.Statev1.RATE_BYTES <= output.Length) // last 16 bytes are the tag
+            {
+                state.StripBlock(Ciphertext.Slice(d_ptr, Gimli24.Statev1.RATE_BYTES), ref out_slice, d_ptr);
+                d_ptr += Gimli24.Statev1.RATE_BYTES;
+            }
+            state.StripLastBlock(Ciphertext.Slice(d_ptr, Ciphertext.Length % Gimli24.Statev1.RATE_BYTES), ref out_slice, d_ptr);
+
+            byte[] local_tag = state.Squeeze();
+            Int32 verify = 0;
+            for (byte v = 0; v < TAG_BYTES; v++)
+                verify |= local_tag[v] ^ Tag[v];
+            if (verify == 0)
+                return output;
+            else
+                return null;
+        }
+    }
+}
